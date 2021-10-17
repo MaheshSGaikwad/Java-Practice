e1f7c9d10d71dccbcee53bf904f4c0cb3253b77f
Mahesh Gaikwad <maheshgaikwad193@gmail.com>
2021-10-17 09:46:28 +0530

    updated through intellJ

--- test.java
+++ test.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class NumCheck {
+public class test {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number");
-		int n;
-		n=sc.nextInt();
-		if(n%2==0)
+		System.out.println("Enter the number to be checked");
+		int num;
+		num=sc.nextInt();
+		if(num%2==0)
 			System.out.println("even number");
 		else
 			System.out.println("odd number");
